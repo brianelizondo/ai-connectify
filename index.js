43df1223bb95c7892b5c0efe179032a05bcf4327
Brian Elizondo <brian_elizondo@msn.com>
2023-05-01 19:26:48 -0400

    files updated with comments about classes, functions, params and returns

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
 */
 'use strict';
 
+/**
+* AI-Connectify is a library that allows users to interact with various AI models using different APIs. 
+* Currently supported models include TensorFlow Node, ChatGPT and DALL-E.
+* @module AIConnectify
+*/
 import AIConnectify from './lib/ai-connectify';
 
+// this module should only have a default export
 export default AIConnectify;
