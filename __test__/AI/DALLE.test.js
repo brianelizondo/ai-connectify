e8b72611f545bc533545a0b94d46ef2482172a6e
Brian Elizondo <brian_elizondo@msn.com>
2023-08-22 13:58:27 -0400

    update unit tests for each AI classes

--- __test__/AI/DALLE.test.js
+++ __test__/AI/DALLE.test.js
@@ -1,42 +1,48 @@
 import AIConnectifyError from '../../lib/AIConnectifyError';
 import DALLE from '../../lib/connectors/AI/DALLE';
 
-const dallE = new DALLE("OPENAI-API-KEY");
+const dallE = new DALLE("OPEN-AI-KEY");
 const dallePromptTest = "A cute baby sea otter";
 
-describe("DALLE AI class", () => { 
-    it("Tests if listModels function returns the list of models", async () => {
-        const listModels = await dallE.listModels();
-        expect(listModels).toEqual(expect.any(Array));
+describe("TensorFlow AI class", () => { 
+    describe("Successful test cases", () => { 
+        it("Tests if listModels function returns the list of models", async () => {
+            const listModels = await dallE.listModels();
+            expect(listModels).toEqual(expect.any(Array));
+        });
+    
+        it("Tests if reatrieveModel function returns the list of models", async () => {
+            const modelDetails = await dallE.reatrieveModel("text-davinci-003");
+            expect(modelDetails).toEqual(expect.any(Object));
+        });
+    
+        it("Tests if createImage function returns array with url's of the new image created", async () => {
+            jest.setTimeout(10000);
+            const newImage = await dallE.createImage(dallePromptTest);
+            expect(newImage).toEqual(expect.any(Array));
+        });
     });
 
-    it("Tests if reatrieveModel function returns the list of models", async () => {
-        const modelDetails = await dallE.reatrieveModel("text-davinci-003");
-        expect(modelDetails).toEqual(expect.any(Object));
-    });
+    describe("AIConnectifyError is throw in methods", () => { 
+        it("Tests is thrown error in reatrieveModel function when the model id is an empty string", async () => {
+            await expect(dallE.reatrieveModel()).rejects.toThrow(AIConnectifyError);
+            await expect(dallE.reatrieveModel("")).rejects.toThrow(AIConnectifyError);
+        });
 
-    it("Tests if createImage function returns array with url's of the new image created", async () => {
-        jest.setTimeout(10000);
-        const newImage = await dallE.createImage(dallePromptTest);
-        expect(newImage).toEqual(expect.any(Array));
-    });
-});
+        it("Tests is thrown error in createImage function when the prompt is an empty string", async () => {
+            await expect(dallE.createImage("")).rejects.toThrow(AIConnectifyError);
+        });
 
-describe("AIConnectifyError is throw in DALLE AI class", () => {
-    it("Tests is thrown error in reatrieveModel function when the model id is an empty string", async () => {
-        await expect(dallE.reatrieveModel()).rejects.toThrow(AIConnectifyError);
-        await expect(dallE.reatrieveModel("")).rejects.toThrow(AIConnectifyError);
-    });
-    it("Tests is thrown error in createImage function when the prompt is an empty string", async () => {
-        await expect(dallE.createImage("")).rejects.toThrow(AIConnectifyError);
-    });
-    it("Tests is thrown error in createImageEdit function when the image is empty", async () => {
-        await expect(dallE.createImageEdit("", "prompt test")).rejects.toThrow(AIConnectifyError);
-    });
-    it("Tests is thrown error in createImageEdit function when the prompt is an empty string", async () => {
-        await expect(dallE.createImageEdit("imageTest", "")).rejects.toThrow(AIConnectifyError);
-    });
-    it("Tests is thrown error in createImageVariation function when the image is empty", async () => {
-        await expect(dallE.createImageVariation("")).rejects.toThrow(AIConnectifyError);
+        it("Tests is thrown error in createImageEdit function when the image is empty", async () => {
+            await expect(dallE.createImageEdit("", "prompt test")).rejects.toThrow(AIConnectifyError);
+        });
+
+        it("Tests is thrown error in createImageEdit function when the prompt is an empty string", async () => {
+            await expect(dallE.createImageEdit("imageTest", "")).rejects.toThrow(AIConnectifyError);
+        });
+
+        it("Tests is thrown error in createImageVariation function when the image is empty", async () => {
+            await expect(dallE.createImageVariation("")).rejects.toThrow(AIConnectifyError);
+        });
     });
 });
--- __test__/AI/TensorFlowNode.test.js
+++ __test__/AI/TensorFlowNode.test.js
@@ -4,57 +4,61 @@ import TensorFlowNode from '../../lib/connectors/AI/TensorFlowNode';
 const tfNode = new TensorFlowNode().tf;
 
 describe("TensorFlow AI class", () => { 
-    it("Tests if tensor method returns a tensor object", async () => {
-        const newTensor = await tfNode.tensor([1, 2, 3, 4]);
-        expect(newTensor).toEqual(expect.any(Object));
-        expect(newTensor.size).toEqual(4);
-    });
-    it("Tests if tensor1d method returns a tensor1d object", async () => {
-        const newTensor = await tfNode.tensor1d([1, 2, 3]);
-        expect(newTensor).toEqual(expect.any(Object));
-        expect(newTensor.size).toEqual(expect.any(Number));
-    });
-    it("Tests if buffer method returns a buffer object", async () => {
-        const newBuffer = tfNode.buffer([2, 2]);
-        newBuffer.set(3, 0, 0);
-        newBuffer.set(5, 1, 0);
-        newBuffer.toTensor();
-        expect(newBuffer).toEqual(expect.any(Object));
-        expect(newBuffer.size).toEqual(4);
+    describe("Successful test cases", () => { 
+        it("Tests if tensor method returns a tensor object", async () => {
+            const newTensor = await tfNode.tensor([1, 2, 3, 4]);
+            expect(newTensor).toEqual(expect.any(Object));
+            expect(newTensor.size).toEqual(4);
+        });
+
+        it("Tests if tensor1d method returns a tensor1d object", async () => {
+            const newTensor = await tfNode.tensor1d([1, 2, 3]);
+            expect(newTensor).toEqual(expect.any(Object));
+            expect(newTensor.size).toEqual(expect.any(Number));
+        });
+
+        it("Tests if buffer method returns a buffer object", async () => {
+            const newBuffer = tfNode.buffer([2, 2]);
+            newBuffer.set(3, 0, 0);
+            newBuffer.set(5, 1, 0);
+            newBuffer.toTensor();
+            expect(newBuffer).toEqual(expect.any(Object));
+            expect(newBuffer.size).toEqual(4);
+        });
+    
+        it("Tests if model sequential method returns a model sequential object", async () => {
+            const newModel = tfNode.sequential();
+            newModel.add(tfNode.layers.dense({units: 32, inputShape: [50]}));
+            newModel.add(tfNode.layers.dense({units: 4}));
+            const inspectModel = newModel.outputs[0].shape;
+    
+            expect(inspectModel).toEqual(expect.any(Array));
+            expect(inspectModel[0]).toEqual(null);
+            expect(inspectModel[1]).toEqual(expect.any(Number));
+        });
+    
+        it("Tests if model method returns a model object", async () => {
+            const input = tfNode.input({shape: [5]});
+            const denseLayer1 = tfNode.layers.dense({units: 10, activation: 'relu'});
+            const denseLayer2 = tfNode.layers.dense({units: 4, activation: 'softmax'});
+            const output = denseLayer2.apply(denseLayer1.apply(input));
+            const newModel = tfNode.model({inputs: input, outputs: output});
+            const predictModel = newModel.predict(tfNode.ones([2, 5]));
+            
+            expect(predictModel).toEqual(expect.any(Object));
+            expect(predictModel.shape).toEqual([2,4]);
+            expect(predictModel.size).toEqual(expect.any(Number));
+        });
     });
 
-    it("Tests if model sequential method returns a model sequential object", async () => {
-        const newModel = tfNode.sequential();
-        newModel.add(tfNode.layers.dense({units: 32, inputShape: [50]}));
-        newModel.add(tfNode.layers.dense({units: 4}));
-        const inspectModel = newModel.outputs[0].shape;
-
-        expect(inspectModel).toEqual(expect.any(Array));
-        expect(inspectModel[0]).toEqual(null);
-        expect(inspectModel[1]).toEqual(expect.any(Number));
-    });
-
-    it("Tests if model method returns a model object", async () => {
-        const input = tfNode.input({shape: [5]});
-        const denseLayer1 = tfNode.layers.dense({units: 10, activation: 'relu'});
-        const denseLayer2 = tfNode.layers.dense({units: 4, activation: 'softmax'});
-        const output = denseLayer2.apply(denseLayer1.apply(input));
-        const newModel = tfNode.model({inputs: input, outputs: output});
-        const predictModel = newModel.predict(tfNode.ones([2, 5]));
-        
-        expect(predictModel).toEqual(expect.any(Object));
-        expect(predictModel.shape).toEqual([2,4]);
-        expect(predictModel.size).toEqual(expect.any(Number));
-    });
-});
-
-describe("AIConnectifyError is throw in TensorFlowNode AI class", () => {
-    it("Tests is thrown error in a function when the argument is not provided", async () => {
-        try {
-            await tfNode.tensor();
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toEqual(expect.any(String));
-        }
+    describe("AIConnectifyError is throw in methods", () => { 
+        it("Tests is thrown error in a function when the argument is not provided", async () => {
+            try {
+                await tfNode.tensor();
+            } catch (error) {
+                expect(error).toBeInstanceOf(Error);
+                expect(error.message).toEqual(expect.any(String));
+            }
+        });
     });
 });
