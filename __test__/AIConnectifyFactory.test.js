bba3bd5f4027c283f69c271fc85a79449aa9ecde
Brian Elizondo <brian_elizondo@msn.com>
2024-09-29 13:14:00 -0400

    Testing - Updated testing cases for AIConnectifyFactory

--- __test__/AIConnectifyFactory.test.js
+++ __test__/AIConnectifyFactory.test.js
@@ -1,29 +1,37 @@
+import 'dotenv/config';
 import AIConnectifyError from '../lib/AIConnectifyError';
-import getAIInstance from '../lib/connectors/AIConnectifyFactory';
+import { createAIInstance } from '../lib/connectors/AIConnectifyFactory';
+import TensorFlow from '../lib/connectors/AI/TensorFlow/TensorFlow';
+import ChatGPT from '../lib/connectors/AI/ChatGPT/ChatGPT';
 
-import ChatGPT from '../lib/connectors/AI/ChatGPT';
-import TensorFlowNode from '../lib/connectors/AI/TensorFlowNode';
+const aiServiceWithKey = "ChatGPT";
+const aiServiceWithoutKey = "TensorFlow";
+const aiServiceKey = process.env.AI_SERVICE_APIKEY || "AI-SERVICE-APIKEY";
 
-describe("AIConnectifyFactory function", () => {
-    it("Tests if getAIInstance returns the correct instance for a supported AI with apiKey", () => {
-        const aiInstance = getAIInstance("ChatGPT", "testApiKey");
-        expect(aiInstance).toBeInstanceOf(ChatGPT);
+describe("AIConnectifyFactory", () => {
+    describe("createAIInstance function", () => {
+        it("Tests if returns the correct instance for a supported AI without apiKey", () => {
+            const aiInstance = createAIInstance(aiServiceWithoutKey);
+            expect(aiInstance).toBeInstanceOf(TensorFlow);
+        });
+        it("Tests if returns the correct instance for a supported AI with apiKey", () => {
+            const aiInstance = createAIInstance(aiServiceWithKey, aiServiceKey);
+            expect(aiInstance).toBeInstanceOf(ChatGPT);
+        });
     });
-    it("Tests if getAIInstance returns the correct instance for a supported AI without apiKey", () => {
-        const aiInstance = getAIInstance("TensorFlowNode");
-        expect(aiInstance).toEqual(expect.any(Object));
-        
-    });
-});
-
-describe("AIConnectifyError is throw in getAIInstance function", () => {
-    it("Tests is thrown error when AI parameter is missing", () => {
-        expect(() => getAIInstance()).toThrow(AIConnectifyError);
-    });
-    it("Tests is thrown error when an invalid AI parameter is provided", () => {
-        expect(() => getAIInstance("testInvalidAI")).toThrow(AIConnectifyError);
-    });
-    it("Tests is thrown error if an apiKey is required but not provided", () => {
-        expect(() => getAIInstance("ChatGPT")).toThrow(AIConnectifyError);
+    
+    describe("Throw the AIConnectifyError error", () => {
+        it("Tests when AI parameter is missing", () => {
+            expect(() => createAIInstance()).toThrow(AIConnectifyError);
+        });
+        it("Tests if an apiKey is required but is missing", () => {
+            expect(() => createAIInstance(aiServiceWithKey)).toThrow(AIConnectifyError);
+        });
+        it("Tests when an invalid AI parameter is provided", () => {
+            expect(() => createAIInstance("testInvalidAI")).toThrow(AIConnectifyError);
+        });
+        it("Tests when an invalid apiKey parameter is provided", () => {
+            expect(() => createAIInstance("******")).toThrow(AIConnectifyError);
+        });
     });
 });
